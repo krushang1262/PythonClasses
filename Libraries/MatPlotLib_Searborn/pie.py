47fb0e0b59e86b84478bb324064b6dbd789c56bf
krushangtops <krushangtops@gmail.com>
2026-04-09 15:04:21 +0530

    all

--- Libraries/MatPlotLib_Searborn/pie.py
+++ Libraries/MatPlotLib_Searborn/pie.py
@@ -5,9 +5,9 @@ import seaborn as sbn
 
 df1 = pd.read_csv('~/Desktop/DataSheet/diamonds.csv')
 
-# ct = df1['cut'].value_counts()
-# plt.pie(ct, labels=ct.index)
-# plt.show()
+ct = df1['cut'].value_counts()
+plt.pie(ct, labels=ct.index)
+plt.show()
 
 df = df1.groupby('cut')['price'].sum().reset_index()
 print(df)
