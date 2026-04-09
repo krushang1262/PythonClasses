fcf73de2717212ec2d2edfb75abb0143e944d01e
krushangtops <krushangtops@gmail.com>
2026-04-09 09:57:06 +0530

    all

--- Libraries/Pandas/Task2.py
+++ Libraries/Pandas/Task2.py
@@ -14,6 +14,7 @@ removeDup2 = Sales_person_Table.drop_duplicates(subset=Sales_person_Table).sort_
 Shipment_Table = df[['ShipmentID','SPID','PID','GID','Shipdate','Amount','Boxes','Order_Status']]
 removeDup3 = Shipment_Table.drop_duplicates(subset=Shipment_Table).sort_values(by='ShipmentID', ascending=True)
 
+
 loction_s = Shipment_Table.groupby('GID')['Amount'].sum().reset_index()
 Report1 = pd.merge(removeDup1,loction_s,on="GID",how="inner")
 Report1 = Report1[['Geo','Amount']]
@@ -27,5 +28,7 @@ r1['Sales & Costing'] = r1['Boxes'] * r1['Cost_per_box']
 topselling = removeDup.value_counts().head(5)
 print(topselling,"\n")
 
-
+Shipment_Table['Order_Status'] = Shipment_Table['Order_Status'].count()
+countOrderStatus = Shipment_Table['Order_Status'].reset_index()
+print(countOrderStatus)
 
