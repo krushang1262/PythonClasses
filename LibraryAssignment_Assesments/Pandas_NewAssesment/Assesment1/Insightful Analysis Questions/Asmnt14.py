f71828b03a539f7fbb734a8a3219fc2643c12d0d
krushangtops <krushangtops@gmail.com>
2026-04-14 19:21:26 +0530

    all

--- LibraryAssignment_Assesments/Pandas_NewAssesment/Assesment1/Insightful Analysis Questions/Asmnt14.py
+++ LibraryAssignment_Assesments/Pandas_NewAssesment/Assesment1/Insightful Analysis Questions/Asmnt14.py
@@ -3,3 +3,5 @@ import pandas as pd
 # 14. Which indicator has the highest correlation with stock close price?
 df = pd.read_csv('~/Desktop/AssignmentData/finance_economics_dataset.csv')
 
+corval = df.corr(numeric_only=True)['Close Price']
+print(corval)
