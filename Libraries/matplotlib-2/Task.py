b783ab9dba568760315db0ece7eb476e938374e9
krushangtops <krushangtops@gmail.com>
2026-07-13 15:00:22 +0530

    done task

--- Libraries/matplotlib-2/Task.py
+++ Libraries/matplotlib-2/Task.py
@@ -28,3 +28,37 @@ df3 = df.groupby('Payment_Method')['Purchase_Amount'].sum()
 print(df3,'\n')
 plt.pie(df3,labels=df3.index, autopct='%.2f%%')
 plt.show()
+
+df['age-group'] = np.where(
+    (df['Age']> 0) & (df['Age']<=20), "0-17",
+    np.where(
+        (df['Age']>20) & (df['Age']<=30), "20-30",
+    np.where(
+        (df['Age']>31) & (df['Age']<=40), "31-40",
+        
+    np.where(
+        (df['Age']>41) & (df['Age']<=50), "41-50",
+        np.where(
+        (df['Age']>50) & (df['Age']<=60), "50-60",
+        np.where(
+        (df['Age']>61) & (df['Age']<=80), "60-80",
+        "60+"
+        )
+        )
+    )   
+    )     
+    )
+)
+
+ttl_amt = df.groupby('age-group')['Purchase_Amount'].sum().reset_index()
+ttl_amt = ttl_amt.sort_values(by='age-group', ascending=True)
+print(ttl_amt)
+
+sbn.barplot(data=ttl_amt, x='age-group', y='Purchase_Amount')
+plt.show()
+
+list5 = df.groupby('User_Name')['Transaction_ID'].count().reset_index().tail(5)
+print(list5)
+
+sbn.barplot(data=list5, x='User_Name', y=list5['Transaction_ID'])
+plt.show()
