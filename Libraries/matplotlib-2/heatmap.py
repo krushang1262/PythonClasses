5f6e9826209e5f062b5bf30ff573e725ed959c82
krushangtops <krushangtops@gmail.com>
2026-07-15 20:53:33 +0530

    done

--- Libraries/matplotlib-2/heatmap.py
+++ Libraries/matplotlib-2/heatmap.py
@@ -7,8 +7,8 @@ df = pd.read_csv('~/Desktop/DataSheet/diamonds.csv')
 df1 = df.groupby(['cut','color'])['price'].count().unstack()
 print(df1)
 
-# sbn.heatmap(data=df1,cmap='coolwarm', annot=True, fmt='.0f', linewidths=1)
-# plt.show()
+sbn.heatmap(data=df1,cmap='coolwarm', annot=True, fmt='.0f', linewidths=1)
+plt.show()
 
 sbn.pairplot(df,vars=['carat','depth','table','price'],palette='Set2', hue='cut')
 plt.show()
