1e7823fe52f294173917159a21407935481fd758
krushangtops <krushangtops@gmail.com>
2026-07-21 16:29:21 +0530

    complete assignment 2

--- demo.py
+++ demo.py
@@ -1,22 +1,8 @@
 import pandas as pd 
 import numpy as np
 
-df = pd.read_csv('~/Downloads/zomato_order_dataset.csv')
-pop_mean = df['Order Amount'].mean()
-pop_median = df['Order Amount'].median()
-pop_mode = df['Order Amount'].mode()[0]
+df = pd.read_csv('~/Downloads/spotify_timeseries_dataset.csv')
 
-print("--- Population Metrics ---")
-print(f"Population Mean: {pop_mean}")
-print(f"Population Median: {pop_median}")
-print(f"Population Mode: {pop_mode}\n")
-
-sample = df['Order Amount'].head(5)
-sample_mean = sample.mean()
-
-print("--- Sample Metrics (First 5 Rows) ---")
-print(f"Sample Values: {sample.tolist()}")
-print(f"Sample Mean: {sample_mean}\n")
-
-diff = abs(pop_mean - sample_mean)
-print(diff)
+df1 = df.groupby('Song Name')['Play Count per Day'].sum().reset_index()
+df1.rename(columns={'Play Count per Day':'Total Plays'}, inplace=True)
+print(df1)
