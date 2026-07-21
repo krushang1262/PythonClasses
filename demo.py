27ee3dced6514d11c23d067c194ed546e7b6beb9
krushangtops <krushangtops@gmail.com>
2026-07-21 17:14:24 +0530

    complete asignment 3 stats

--- demo.py
+++ demo.py
@@ -1,8 +1,6 @@
 import pandas as pd 
 import numpy as np
 
-df = pd.read_csv('~/Downloads/spotify_timeseries_dataset.csv')
-
-df1 = df.groupby('Song Name')['Play Count per Day'].sum().reset_index()
-df1.rename(columns={'Play Count per Day':'Total Plays'}, inplace=True)
-print(df1)
+amount  = [100, 120, 105, 110, 5000, 115, 108]
+print("average upi transaction (in ₹): ", np.mean(amount))
+print("median upi transaction (in ₹): ", np.median(amount))
