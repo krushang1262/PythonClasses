c8beeebbec5cbd3f1f9a2058891f098b99f0f509
krushangtops <krushangtops@gmail.com>
2026-07-21 21:20:43 +0530

    done

--- demo.py
+++ demo.py
@@ -1,18 +1,10 @@
 import pandas as pd 
-import numpy as np
+import matplotlib.pyplot as plt
 
-order_amounts = [180, 250, 310, 150, 420, 2200, 290, 340, 190, 210, 450, 380, 5100, 280, 350]
+screen_time = [28, 32, 35, 26, 30, 68, 31, 29]
 
-def val_outliers(data):
-    q1 = np.percentile(data, 25)
-    q3 = np.percentile(data, 75)
-
-    IQR = q3-q1
-    LB = q1 - (IQR * 1.5)
-    UB = q3 + (IQR * 1.5)
-
-    outlier = [x for x in data if x < LB or x > UB]
-    print(outlier)
-    return outlier
-
-val_outliers(order_amounts)
+plt.title("weekly screen time (in hours) past 8 weeks")
+plt.ylabel("screen time (in hours)")
+plt.boxplot(screen_time)
+plt.xticks([1],['My Screen Time'])
+plt.show()
