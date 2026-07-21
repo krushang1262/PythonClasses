133c69fe72835ad2b22a5d63e7b0116b1744036b
krushangtops <krushangtops@gmail.com>
2026-07-21 21:20:24 +0530

    complete 5 assignements

--- demo.py
+++ demo.py
@@ -1,7 +1,18 @@
 import pandas as pd 
 import numpy as np
 
-upi_amounts = [150, 2200, 80, 350, 120, 4500, 250, 180]
+order_amounts = [180, 250, 310, 150, 420, 2200, 290, 340, 190, 210, 450, 380, 5100, 280, 350]
 
-print("Variance: ", np.var(upi_amounts),'\n')
-print("standard Deviations: ", np.std(upi_amounts))
+def val_outliers(data):
+    q1 = np.percentile(data, 25)
+    q3 = np.percentile(data, 75)
+
+    IQR = q3-q1
+    LB = q1 - (IQR * 1.5)
+    UB = q3 + (IQR * 1.5)
+
+    outlier = [x for x in data if x < LB or x > UB]
+    print(outlier)
+    return outlier
+
+val_outliers(order_amounts)
