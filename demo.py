4e01386c09d65984de118a27a8a7d45c2d7a2466
krushangtops <krushangtops@gmail.com>
2026-07-22 16:16:15 +0530

    complete 6 assignments

--- demo.py
+++ demo.py
@@ -1,10 +1,3 @@
 import pandas as pd 
 import matplotlib.pyplot as plt
 
-screen_time = [28, 32, 35, 26, 30, 68, 31, 29]
-
-plt.title("weekly screen time (in hours) past 8 weeks")
-plt.ylabel("screen time (in hours)")
-plt.boxplot(screen_time)
-plt.xticks([1],['My Screen Time'])
-plt.show()
