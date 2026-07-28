4de36232089a9d8c6e826008a6b70435645dc31d
krushangtops <krushangtops@gmail.com>
2026-07-28 16:44:43 +0530

    complete session 9

--- demo.py
+++ demo.py
@@ -2,4 +2,16 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np 
 
-from scipy.stats import poisson
+import matplotlib.pyplot as plt
+
+
+data = np.random.normal(loc=500,scale=100,size=1000)
+mean = np.mean(data)
+stdDev = np.std(data)
+
+print("mean: ",mean,'\n',"Standaed Deviations",stdDev)
+
+plt.hist(x=data, bins=30)
+plt.show()
+
+
