4187dc5b97cbfbd07472cdb2f2ed23da2ecbff2b
krushangtops <krushangtops@gmail.com>
2026-08-14 18:02:48 +0530

    16 assignment done

--- demo.py
+++ demo.py
@@ -1,29 +1,6 @@
-import numpy as np
-import matplotlib.pyplot as plt
-from scipy.stats import norm
+import pandas as pd 
 
-# Create Z values
-z = np.linspace(-4, 4, 1000)
-
-# Standard normal distribution
-y = norm.pdf(z, 0, 1)
-
-# Plot the curve
-plt.plot(z, y)
-
-# Shade area between Z = -1 and Z = 1
-z_fill = np.linspace(-1, 1, 500)
-y_fill = norm.pdf(z_fill, 0, 1)
-
-plt.fill_between(z_fill, y_fill, alpha=0.5)
-
-# Add lines at Z = -1 and Z = 1
-plt.axvline(-1, linestyle='--')
-plt.axvline(1, linestyle='--')
-
-# Labels
-plt.xlabel("Z-score")
-plt.ylabel("Probability Density")
-plt.title("Standard Normal Distribution")
-
-plt.show()
+df1 = pd.read_csv('~/Desktop/gameAdd/gaming_addiction.csv')
+df = pd.DataFrame(df1)
+print("strongly positive corelation: ",df["Order_Value"].corr(df["Delivery_Distance"]))
+print("strongly positive corelation: ",df["Delivery_Charge"].corr(df["Delivery_Distance"]))
